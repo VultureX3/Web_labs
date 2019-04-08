323792d40eb440c3d91e162f65eab04cdd6d8865
VultureX3 <ikhalepsky@gmail.com>
2019-04-08 10:15:30 +0300

    lab3 started; canvas done

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -9,7 +9,7 @@ function setAttributes(el, attrs) {
 setAttributes(canvas, {
     'id': 'canvas',
     'width': '600px',
-    'height': '300px',
+    'height': '600px',
     'style': 'border: 2px solid'
 })
 
@@ -17,6 +17,16 @@ var body = document.body;
 body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
-var image = new Image();
-image.src = '../lab2/img/1.jpg';
-ctx.drawImage(image, 0, 0)
+var image1 = new Image();
+image1.src = 'https://unsplash.com/collections/2203755/300x300';
+image1.setAttribute("crossOrigin", "Anonymous");
+ctx.drawImage(image1, 0, 0, canvas.width / 2, canvas.height / 2);
+var image2 = new Image();
+image2.src = '../lab2/img/2.jpg';
+ctx.drawImage(image2, canvas.width / 2, 0, canvas.width / 2, canvas.height / 2);
+var image3 = new Image();
+image3.src = '../lab2/img/3.jpg';
+ctx.drawImage(image3, 0, canvas.height / 2, canvas.width / 2, canvas.height / 2);
+var image4 = new Image();
+image4.src = '../lab2/img/logo.jpg';
+ctx.drawImage(image4, canvas.width / 2, canvas.height / 2, canvas.width / 2, canvas.height / 2);
